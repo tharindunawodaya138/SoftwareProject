d64446ebd4b26812e0be45bb6ad1746cb938bee1
tharindu nawodaya <tharindunawodaya138@gmail.com>
2020-11-19 05:10:46 +0530

    AddCors

---
+++ Govimithuro/Migrations/20201118233741_AddCors.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Govimithuro.Migrations
+{
+    public partial class AddCors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Govimithuro/Startup.cs
+++ Govimithuro/Startup.cs
@@ -34,6 +34,14 @@ namespace Govimithuro
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseCors(options =>
+            options.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
