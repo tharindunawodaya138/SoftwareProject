9905e5882c25740fd256ecbefceddef48daee72c
bcHansani <67742407+bcHansani@users.noreply.github.com>
2021-02-21 00:13:24 +0530

    Update BillingInfo Model

--- Govimithuro/Migrations/20210220181938_UpdateBillingInfo.Designer.cs
+++ Govimithuro/Migrations/20210220181938_UpdateBillingInfo.Designer.cs
@@ -3,14 +3,16 @@ using Govimithuro.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Govimithuro.Migrations
 {
     [DbContext(typeof(GovimithuroDbContext))]
-    partial class GovimithuroDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210220181938_UpdateBillingInfo")]
+    partial class UpdateBillingInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
